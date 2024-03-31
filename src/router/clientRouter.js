ba9b37ede295430bf290fe1f3e863b6e64492b98
Facu V <facundovinales@hotmail.com>
2024-03-31 20:45:10 -0300

    BackEnd Caso de uso 1

--- src/router/clientRouter.js
+++ src/router/clientRouter.js
@@ -1,5 +1,6 @@
 const Client = require("../models/Client")
 const User = require("../models/User")
+const sequelize = require('../config/db.js');
 
 const router = require("express").Router()
 
@@ -30,11 +31,43 @@ router.get("/clients/:client_id", async (req, res) => {
 })
 
 router.post("/clients", async (req, res) => {
-  try {
+  sequelize.transaction(async (t) => {
     const userData = req.body
 
-    const newClient = await Client.create({
-      user: {
+    // Crea el usuario
+    const user = await User.create({
+      foto: userData.foto,
+      nombre_usuario: userData.nombre_usuario,
+      contraseña: userData.contraseña,
+      direccion: userData.direccion,
+      fecha_nacimiento: userData.fecha_nacimiento,
+      email: userData.email,
+      telefono: userData.telefono,
+      calificacion: userData.calificacion
+    }, { transaction: t });
+  
+    // Crea el cliente asociado al usuario recién creado
+    const client = await Client.create({
+      id: user.id // Asigna el ID del usuario recién creado
+    }, { transaction: t });
+    res.status(201).json({
+      ok: true,
+      status: 201,
+      message: "Cliente creado exitosamente",
+    });
+  }).catch((error) => {
+    // Si algo falla, revierte la transacción
+    res.status(500).send('Error al crear cliente');
+    console.error('Error al crear usuario y cliente:', error);
+  });
+})
+
+router.put("/clients/:client_id", async (req, res) => {
+  try {
+    const id = req.params.client_id
+    const userData = req.body
+    const updateClient = await User.update(
+      {
         foto: userData.foto,
         nombre_usuario: userData.nombre_usuario,
         contraseña: userData.contraseña,
@@ -43,38 +76,51 @@ router.post("/clients", async (req, res) => {
         email: userData.email,
         telefono: userData.telefono,
         calificacion: userData.calificacion
+      },
+      {
+        where: {
+          id: id
         }
-    }, {
-      include: [{
-        association: Client.User
-      }]
-    });
-
-    res.status(201).json({
-      ok: true,
-      status: 201,
-      message: "Cliente creado exitosamente",
-      body: {
-        user: newUser,
-        client: newClient
       }
-    });
+    )
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Cliente modificado exitosamente",
+    })
   } catch (error) {
-    console.error("Error al crear el cliente:", error);
-    res.status(500).json({
-      ok: false,
-      status: 500,
-      message: "Error al crear el cliente"
-    });
+      res.status(500).send('Error al modificar cliente')
+      console.error('Error al modificar cliente:', error)
   }
 })
 
-router.put("/clients/:client_id", (req, res) => {
-  res.send("Modificar")
-})
+router.delete("/clients/:client_id", async (req, res) => {
+  sequelize.transaction(async (t) => {
+    const id = req.params.client_id
 
-router.delete("/clients/:client_id", (req, res) => {
-  res.send("Eliminar")
+    const deleteClient = await Client.destroy({
+      where: {
+        id: id
+      },
+      transaction: t
+    })
+
+    const deleteUser = await User.destroy({
+      where: {
+        id: id
+      },
+      transaction: t
+    })
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Cliente eliminado exitosamente",
+    });
+  }).catch((error) => {
+    // Si algo falla, revierte la transacción
+    res.status(500).send('Error al eliminar cliente');
+    console.error('Error al eliminar cliente:', error);
+  });
 })
 
 module.exports = router
--- src/router/walkerRouter.js
+++ src/router/walkerRouter.js
@@ -1,6 +1,6 @@
 const User = require("../models/User")
 const Walker = require("../models/Walker")
-
+const sequelize = require('../config/db.js');
 const router = require("express").Router()
 
 router.get("/walkers", async (req, res) => {
@@ -30,15 +30,111 @@ router.get("/walkers/:walker_id", async (req, res) => {
 })
 
 router.post("/walkers", (req, res) => {
-  res.send("Agregar")
+  sequelize.transaction(async (t) => {
+    const userData = req.body
+
+    // Crea el usuario
+    const user = await User.create({
+      foto: userData.foto,
+      nombre_usuario: userData.nombre_usuario,
+      contraseña: userData.contraseña,
+      direccion: userData.direccion,
+      fecha_nacimiento: userData.fecha_nacimiento,
+      email: userData.email,
+      telefono: userData.telefono,
+      calificacion: userData.calificacion
+    }, { transaction: t });
+  
+    // Crea el cliente asociado al usuario recién creado
+    const walker = await Walker.create({
+      id: user.id, // Asigna el ID del usuario recién creado
+      fotos: userData.fotos
+    }, { transaction: t });
+    res.status(201).json({
+      ok: true,
+      status: 201,
+      message: "Paseador creado exitosamente",
+    });
+  }).catch((error) => {
+    // Si algo falla, revierte la transacción
+    res.status(500).send('Error al crear paseador');
+    console.error('Error al crear paseador:', error);
+  });
 })
 
 router.put("/walkers/:walker_id", (req, res) => {
-  res.send("Modificar")
+  sequelize.transaction(async (t) => {
+    const userData = req.body
+    const id = req.params.walker_id
+
+    // Modifico el usuario
+    const user = await User.update({
+      foto: userData.foto,
+      nombre_usuario: userData.nombre_usuario,
+      contraseña: userData.contraseña,
+      direccion: userData.direccion,
+      fecha_nacimiento: userData.fecha_nacimiento,
+      email: userData.email,
+      telefono: userData.telefono,
+      calificacion: userData.calificacion
+    }, 
+    { 
+      transaction: t,
+      where: {
+        id: id
+      }
+    });
+  
+    // modifica el paseador asociado al usuario
+    const walker = await Walker.update({
+      fotos: userData.fotos
+    }, 
+    { 
+      transaction: t,
+      where: {
+        id: id
+      }
+    });
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Paseador modificado exitosamente",
+    });
+  }).catch((error) => {
+    // Si algo falla, revierte la transacción
+    res.status(500).send('Error al modificar paseador');
+    console.error('Error al modificar paseador:', error);
+  });
 })
 
 router.delete("/walkers/:walker_id", (req, res) => {
-  res.send("Eliminar")
+  sequelize.transaction(async (t) => {
+    const userData = req.body
+    const id = req.params.walker_id
+
+    const deleteWalker = await Walker.destroy({
+      where: {
+        id: id
+      },
+      transaction: t
+    })
+
+    const deleteUser = await User.destroy({
+      where: {
+        id: id
+      },
+      transaction: t
+    })
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Paseador eliminado exitosamente",
+    });
+  }).catch((error) => {
+    // Si algo falla, revierte la transacción
+    res.status(500).send('Error al eliminar paseador');
+    console.error('Error al eliminar paseador:', error);
+  });
 })
 
 module.exports = router
